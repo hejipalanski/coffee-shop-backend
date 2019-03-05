5eb52b9ef21332c65b1c4771c3ea85f623903c5c
Heji Palaña <hnpalana@codedisruptors.com>
2019-03-05 17:08:45 +0800

    adds bulk create and retrieve menu api

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const shopController = require('../controller/shopController');
 
 router.post('/shops', shopController.create);
+router.post('/shops/bulk', shopController.createBulk);
 router.get('/shops/:id', shopController.retrieveById);
+router.post('/shops/:shopId/menu', shopController.createMenuItem);
+router.get('/shops/:shopId/menu', shopController.retrieveMenu);
 router.get('/shops', shopController.retrieveAll);
 router.patch('/shops/:id', shopController.updateById);
 router.delete('/shops/:id', shopController.deleteById);
