330bf8836e75944b40396ee7276ce538abc6eb95
Heji Palaña <hnpalana@codedisruptors.com>
2019-03-05 17:09:32 +0800

    adds modifies models

--- models/coffee.js
+++ models/coffee.js
@@ -3,16 +3,51 @@ module.exports = (sequelize, DataTypes) => {
 	const Coffee = sequelize.define('Coffee', {
 		id: {
 			allowNull: false,
-			primaryKey: true,
 			type: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV4
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+			validate: {
+				isUUID: 4
+			},
+			notNull: {
+				msg: 'UUID cannot be null.'
+			}
 		},
-		name: DataTypes.STRING(50),
-		type: DataTypes.STRING(30),
-		description: DataTypes.STRING
+		name: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+			validate: {
+				len: {
+					args: 5,
+					msg: "Name must be atleast 5 characters in length"
+				}
+			},
+			notNull: {
+				msg: 'Name cannot be null.'
+			}
+		},
+		type: {
+			type: DataTypes.STRING(30),
+			validate: {
+				len: {
+					args: 5,
+					msg: "Type must be atleast 5 characters in length"
+				}
+			},
+			notNull: {
+				msg: 'Name cannot be null.'
+			}
+		},
+		description: {
+			type: DataTypes.STRING
+		},
+		deletedAt: {
+			allowNull: true,
+			type: DataTypes.DATE
+		}
 	}, {});
 	Coffee.associate = function(models) {
-		Coffee.Shop = Coffee.belongsTo(models.Shop, {foreignKey: 'shopId'});
+		Coffee.MenuList = Coffee.belongsToMany(models.MenuList, {through: 'coffee_menu'});
 	};
 	return Coffee;
 };
--- models/shop.js
+++ models/shop.js
@@ -1,13 +1,71 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
 	const Shop = sequelize.define('Shop', {
-		name: DataTypes.STRING,
-		location: DataTypes.STRING,
-		franchisee: DataTypes.STRING,
-		status: DataTypes.STRING
+		id: {
+				allowNull: false,
+				type: DataTypes.UUID,
+				primaryKey: true,
+				defaultValue: DataTypes.UUIDV4,
+				validate: {
+					isUUID: 4
+				},
+				notNull: {
+				msg: 'UUID cannot be null.'
+			}
+		},
+		name: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+			validate: {
+				len: {
+					args: 5,
+					msg: "Name must be atleast 5 characters in length"
+				}
+			},
+			notNull: {
+				msg: 'Name cannot be null.'
+			}
+		},
+		location: {
+			type:	DataTypes.STRING(100),
+			allowNull: false,
+			validate: {
+				len: {
+						args: 10,
+						msg: "Name must be atleast 10 characters in length"
+				}
+			},
+			notNull: {
+				msg: 'Location cannot be null.'
+			}
+		},
+		franchisee: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+			validate: {
+				len: {
+						args: 10,
+						msg: "franchisee must be atleast 10 characters in length"
+				}
+			},
+			notNull: {
+				msg: 'Franchisee cannot be null.'
+			}
+		},
+		status: {
+			type: DataTypes.STRING(10),
+			allowNull: false,
+			defaultValue: 'close',
+			validate: {
+					len: {
+						args: 4,
+						msg: "status must be atleast 4 characters in length"
+				}
+			}
+		}
 	}, {});
 	Shop.associate = function(models) {
-		Shop.Coffee = Shop.hasMany(models.Coffee);
+		Shop.MenuList = Shop.hasOne(models.MenuList);
 	};
 	return Shop;
 };
