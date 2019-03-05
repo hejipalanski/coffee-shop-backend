7166ba2c515fc8ab901e92d81e408d577b8db23a
Heji Palaña <hnpalana@codedisruptors.com>
2019-03-05 17:09:53 +0800

    adds bulk create api

--- routes/coffee.js
+++ routes/coffee.js
@@ -4,10 +4,11 @@ const express = require('express');
 const router = express();
 const coffeeController = require('../controller/coffeeController');
 
-router.post('/shops/:shopId/drinks', coffeeController.create);
-router.get('/shops/:shopId/drinks/:id', coffeeController.retrieveById);
-router.get('/shops/:shopId/drinks', coffeeController.retrieveAll);
-router.patch('/shops/:shopId/drinks/:id', coffeeController.updateById);
-router.delete('/shops/:shopId/drinks/:id', coffeeController.deleteById);
+router.post('/coffees', coffeeController.create);
+router.post('/coffees/bulk', coffeeController.createBulk);
+router.get('/coffees:id', coffeeController.retrieveById);
+router.get('/coffees', coffeeController.retrieveAll);
+router.patch('/coffees/:id', coffeeController.updateById);
+router.delete('/coffees/:id', coffeeController.deleteById);
 
 module.exports = router;
